6a6bb3b89f1d6fb711601feba88c5f861f35163a
Andreas Lundkvist <github@galdiuz.com>
2018-01-19 17:07:00 +0100

    Fixed Date and Datetime array schemas

--- src/Schema/StringType/DateType.php
+++ src/Schema/StringType/DateType.php
@@ -3,10 +3,32 @@
 namespace Webbhuset\Data\Schema\StringType;
 
 use Webbhuset\Data\Schema\StringType;
+use Webbhuset\Data\Schema\TypeConstructor as T;
 
 class DateType extends StringType
 {
     protected $matches = [
         '/^\d{4}-\d{2}-\d{2}$/' => 'Date is not in format "YYYY-MM-DD',
     ];
+
+
+    protected function _toArray()
+    {
+        return [
+            'type'  => 'date',
+            'args'  => [
+                'nullable'      => $this->isNullable,
+            ],
+        ];
+    }
+
+    protected static function _getArraySchema()
+    {
+        return T::Struct([
+            'type' => T::Enum(['date']),
+            'args' => T::Struct([
+                'nullable'      => T::Bool(T::NULLABLE),
+            ])
+        ]);
+    }
 }
